9c672e8cd52ceb13539c09409e4db4788213a390
lfpborba <luisfpb@icloud.com>
2022-10-19 03:45:19 -0300

    feat: get /teams:id - implementing teamsRouter

--- app/backend/src/router/teamsRouter.ts
+++ app/backend/src/router/teamsRouter.ts
@@ -4,5 +4,6 @@ import TeamsController from '../controllers/teamsController';
 const teamsRouter = Router();
 
 teamsRouter.get('/', TeamsController.getAll);
+teamsRouter.get('/:id', TeamsController.getById);
 
 export default teamsRouter;
