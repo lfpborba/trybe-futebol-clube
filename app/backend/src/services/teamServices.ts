e53f5c98dc75dc15eb1b3c22a442971f6a1bbb9f
lfpborba <luisfpb@icloud.com>
2022-10-19 03:44:35 -0300

    feat: GET /teams:id - implementing teamsService

--- app/backend/src/services/teamServices.ts
+++ app/backend/src/services/teamServices.ts
@@ -1,10 +1,18 @@
 import TeamInterface from '../interfaces/teamInterface';
 import TeamModel from '../database/models/team';
+import ValidationErrorHandler from 'src/helpers/ValidationErrorHandler';
 
 export default class TeamsServices {
   static async getAll(): Promise<TeamInterface[]> {
     const teams = await TeamModel.findAll();
 
     return teams;
+  }
+
+  static async getById(id: number): Promise<TeamInterface> {
+    const team = await TeamModel.findByPk(id);
+    if(!team) throw new ValidationErrorHandler(404, 'Team not found')
+
+    return team
   }
 }
