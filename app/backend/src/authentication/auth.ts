61d1b63d3ccf63cef8f444f0a1395f3514beb1f8
lfpborba <luisfpb@icloud.com>
2022-10-18 21:06:43 -0300

    feat: Implementing passwordAuth

--- app/backend/src/authentication/auth.ts
+++ app/backend/src/authentication/auth.ts
@@ -1,21 +1,22 @@
 import * as Jwt from 'jsonwebtoken';
 import * as Bcrypt from 'bcryptjs';
 import UserInterface from '../interfaces/userInterface';
+import ValidationErrorHandler from '../helpers/ValidationErrorHandler';
 import 'dotenv/config';
 
 export default class Authentication {
-  static generateToken = (user: UserInterface): string => {
-    const token = Jwt.sign(
-      user,
-      process.env.JWT_SECRET as Jwt.Secret,
-      { algorithm: 'HS256', expiresIn: '30d' },
-    );
-    return token;
-  };
+  static generateToken(user: UserInterface): string {
+    const jwtToken = Jwt
+      .sign(
+        { data: user },
+        process.env.JWT_SECRET as Jwt.Secret,
+        { algorithm: 'HS256', expiresIn: '30d' },
+      );
+    return jwtToken;
+  }
 
-  static passwordAuthentication = async (password: string, hash: string) => {
-    Bcrypt.compare(password, hash, (err, _res) => {
-      if (err) throw new Error('INCORRECT_PASSWORD');
-    });
-  };
+  static async passwordAuthentication(password: string, hash: string): Promise<void> {
+    const authResult = await Bcrypt.compare(password, hash);
+    if (!authResult) throw new ValidationErrorHandler(401, 'Incorrect email or password');
+  }
 }
