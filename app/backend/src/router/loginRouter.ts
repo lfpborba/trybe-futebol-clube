05469925791a988b27f44d692575b8824e487601
lfpborba <luisfpb@icloud.com>
2022-10-18 21:20:31 -0300

    feat: implementing LoginRouter

--- app/backend/src/router/loginRouter.ts
+++ app/backend/src/router/loginRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import userAuth from '../middlewares/userAuth';
 import LoginController from '../controllers/loginController';
 
 const loginRouter = Router();
 
-const loginController = new LoginController();
+loginRouter.post('/', async (req, res) => {
+  await LoginController.login(req, res);
+});
 
-loginRouter.post('/', loginController.login);
+loginRouter.get('/validate', userAuth.validateToken, LoginController.loginValidate);
 
 export default loginRouter;
