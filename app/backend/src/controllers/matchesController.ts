ecf11a64453e64b04d7f10b886104f7ab3534112
lfpborba <luisfpb@icloud.com>
2022-10-19 04:35:51 -0300

    fix: eslint

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import MatchesServices from '../services/matchesServices';
 
-export default class MatchesController { 
+export default class MatchesController {
   static async getAll(req: Request, res: Response) {
     const matches = await MatchesServices.getAll();
 
--- app/backend/src/services/matchesServices.ts
+++ app/backend/src/services/matchesServices.ts
@@ -1,18 +1,18 @@
 import { FindOptions } from 'sequelize';
-import TeamModel from "../database/models/team";
-import MatchesModel from "../database/models/match";
+import TeamModel from '../database/models/team';
+import MatchesModel from '../database/models/match';
 
 const findAllOptions: FindOptions = {
   include: [
-    {model: TeamModel, as: 'teamHome', attributes: ['teamName']},
-    {model: TeamModel, as: 'teamAway', attributes: ['teamName']},
+    { model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
+    { model: TeamModel, as: 'teamAway', attributes: ['teamName'] },
   ],
-}
+};
 
 export default class MatchesServices {
   static async getAll() {
     const matches = await MatchesModel.findAll(findAllOptions);
 
-    return matches
+    return matches;
   }
 }
