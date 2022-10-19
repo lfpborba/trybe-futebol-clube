6e43a19be4f679e5d4f76fda4cd315e4bd52a31a
lfpborba <luisfpb@icloud.com>
2022-10-19 03:38:01 -0300

    fix: eslint

--- app/backend/src/controllers/teamsController.ts
+++ app/backend/src/controllers/teamsController.ts
@@ -3,7 +3,7 @@ import TeamsServices from '../services/teamServices';
 
 export default class TeamsController {
   static async getAll(_req: Request, res: Response) {
-    const teams = await TeamsServices.getAll()
+    const teams = await TeamsServices.getAll();
 
     return res.status(200).json(teams);
   }
--- app/backend/src/services/teamServices.ts
+++ app/backend/src/services/teamServices.ts
@@ -1,9 +1,9 @@
-import TeamInterface from "../interfaces/teamInterface";
-import TeamModel from "../database/models/team";
+import TeamInterface from '../interfaces/teamInterface';
+import TeamModel from '../database/models/team';
 
 export default class TeamsServices {
   static async getAll(): Promise<TeamInterface[]> {
-    const teams = await TeamModel.findAll()
+    const teams = await TeamModel.findAll();
 
     return teams;
   }
