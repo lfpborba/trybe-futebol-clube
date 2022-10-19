0c477603f9dfa67a50896812d3c41321c7482ce3
lfpborba <luisfpb@icloud.com>
2022-10-19 02:06:14 -0300

    feat: implementing email validation

--- app/backend/src/helpers/loginData.ts
+++ app/backend/src/helpers/loginData.ts
@@ -1,8 +1,12 @@
+import validationErrorHandler from './ValidationErrorHandler';
+
 export default class LoginDTO {
   private _email: string;
   private _password: string;
 
   constructor(email: string, password: string) {
+    if(!email || email === '') throw new validationErrorHandler(400, 'All fields must be filled')
+
     this._email = email;
     this._password = password;
   }
