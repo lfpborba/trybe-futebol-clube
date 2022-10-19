439f92baf36d31a4397b1a189b4ba157f6943822
lfpborba <luisfpb@icloud.com>
2022-10-19 04:34:00 -0300

    feat: GET /matches - implementing matches controller

---
+++ app/backend/src/controllers/matchesController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import MatchesServices from '../services/matchesServices';
+
+export default class MatchesController { 
+  static async getAll(req: Request, res: Response) {
+    const matches = await MatchesServices.getAll();
+
+    return res.status(200).json(matches);
+  }
+}
