7b4fa9bde83d16929c15e8a986615a6be69fad0a
lfpborba <luisfpb@icloud.com>
2022-10-19 02:22:07 -0300

    feat: implementing password validation

--- app/backend/src/helpers/loginData.ts
+++ app/backend/src/helpers/loginData.ts
@@ -5,10 +5,11 @@ export default class LoginDTO {
   private _password: string;
 
   constructor(email: string, password: string) {
-    if (!email || email === '') throw new ValidationErrorHandler(400, 'All fields must be filled');
-
     this._email = email;
     this._password = password;
+
+    this.validateEmail();
+    this.validatePassword();
   }
 
   public get email(): string {
@@ -17,5 +18,15 @@ export default class LoginDTO {
 
   public get password(): string {
     return this._password;
+  }
+
+  private validateEmail(): void {
+    const email = this._email
+    if (!email || email === '') throw new ValidationErrorHandler(400, 'All fields must be filled');
+  }
+
+  private validatePassword(): void {
+    const password = this._password
+    if(!password || password ==='') throw new ValidationErrorHandler(400, 'All fields must be filled');
   }
 }
