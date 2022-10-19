872a679f3da3c682f6534437a3f1fa8486806c17
lfpborba <luisfpb@icloud.com>
2022-10-19 17:19:01 -0300

    feat: create .post router

--- app/backend/src/router/matchesRouter.ts
+++ app/backend/src/router/matchesRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matchesController';
+import Auth from '../middlewares/userAuth';
 
 const matchesRouter = Router();
 
 matchesRouter.get('/', MatchesController.getAll);
+matchesRouter.post('/', Auth.validateToken, MatchesController.create)
 
 export default matchesRouter;
