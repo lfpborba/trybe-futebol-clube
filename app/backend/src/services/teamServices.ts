0373a65474585026a74faca64aba581b3dfdf46a
lfpborba <luisfpb@icloud.com>
2022-10-19 03:33:33 -0300

    feat: endpoint GET /teams - implementing teamsServices

---
+++ app/backend/src/services/teamServices.ts
@@ -0,0 +1,10 @@
+import TeamInterface from "../interfaces/teamInterface";
+import TeamModel from "../database/models/team";
+
+export default class TeamsServices {
+  static async getAll(): Promise<TeamInterface[]> {
+    const teams = await TeamModel.findAll()
+
+    return teams;
+  }
+}
