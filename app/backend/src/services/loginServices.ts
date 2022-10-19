b2ce8e5236ded80e1838a569f7a397ee7f7f37dc
lfpborba <luisfpb@icloud.com>
2022-10-18 21:02:36 -0300

    feat: POST Login - implementing login service

--- app/backend/src/services/loginServices.ts
+++ app/backend/src/services/loginServices.ts
@@ -1,14 +1,20 @@
+import UserInterface from '../interfaces/userInterface';
 import UserModel from '../database/models/user';
+import Auth from '../authentication/auth';
+import LoginData from '../helpers/loginData';
+import ValidationErrorHandler from '../helpers/ValidationErrorHandler';
 
 export default class LoginService {
-  private _UserModel: typeof UserModel;
+  static async login(email: string, password: string) {
+    const loginData = new LoginData(email, password);
+    
+    const user = await UserModel.findOne({ where: { email: loginData.email } });
 
-  constructor() {
-    this._UserModel = UserModel;
+    if (!user) throw new ValidationErrorHandler(401, 'Incorrect email or password');
+
+    await Auth.passwordAuthentication(loginData.password, user.password);
+    
+    const token = Auth.generateToken(user as UserInterface);
+    return token;
   }
-
-  login = async () => {
-    const users = await this._UserModel.findAll();
-    return users;
-  };
 }
